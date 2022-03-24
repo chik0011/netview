dba8c1b6a104527c4774856fd2bc2947c37f0db9
IC-faycal <faycal@inovea-conseil.com>
2022-03-24 21:10:17 +0100

    ADD modal to confirm deleting

--- client/assets/script/scriptCardMovie.js
+++ client/assets/script/scriptCardMovie.js
@@ -18,7 +18,7 @@ fetch(url, options)
     .then((response) => {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
-        const idMovie = urlParams.get('id')
+        const idMovie = urlParams.get('id');
 
         let movie = "";
         response.movies.forEach(element => {
@@ -30,29 +30,53 @@ fetch(url, options)
         document.querySelector(".title").innerHTML = movie.name;
         document.querySelector(".date").innerHTML += movie.date;
         document.querySelector(".description").innerHTML += movie.description;
-        document.querySelector(".img-movie").src = `../assets/img/imgMovies/${movie.img}`
-        console.log(movie);
+        document.querySelector(".img-movie").src = `../assets/img/imgMovies/${movie.img}`;
 
         let delMovie = document.querySelector(".del-movie");
 
         delMovie.addEventListener('click', () => {
-            var myHeadersToDel = new Headers({ 'Content-Type': 'application/json' });
-    
-            var urlDel = '/movie/del';
+            var delMovieConfirm = document.querySelector(".btn-action-del");
 
-            let movie = {
-                idMovie: idMovie
+            delMovieConfirm.addEventListener("click", () => {
+                var myHeadersToDel = new Headers({ 'Content-Type': 'application/json' });
+    
+                var urlDel = '/movie/del';
+
+                let movie = {
+                    idMovie: idMovie
+                }
+
+                var optionsDel = { 
+                    method: 'POST',
+                    headers: myHeadersToDel,
+                    mode: 'cors',
+                    cache: 'default',
+                    body: JSON.stringify(movie)
+                };
+
+                fetch(urlDel, optionsDel);
+            })
+
+            var modal = document.getElementById("myModal");
+            modal.style.display = "block";
+
+            var span = document.getElementsByClassName("close")[0];
+            
+            span.onclick = function() {
+                modal.style.display = "none";
             }
 
-            var optionsDel = { 
-                method: 'POST',
-                headers: myHeadersToDel,
-                mode: 'cors',
-                cache: 'default',
-                body: JSON.stringify(movie)
-            };
+            let closeModal = document.querySelector(".btn-action-close")
 
-            fetch(urlDel, optionsDel);
+            closeModal.addEventListener("click", () => {
+                modal.style.display = "none";
+            });
+
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
         })
     })
 
